048e286955574679b6976f265dc80a3d4955c9dc
apendyala9 <apendya@purdue.edu>
2025-09-07 23:54:13 -0700

    Having issues with the AI chatbot since I'm using a new model, will work on fixing its generated hints

--- Backend/app.py
+++ Backend/app.py
@@ -2,6 +2,7 @@ from flask import request, jsonify, Flask
 from db_setup import connect_db
 from flask_cors import CORS
 import sqlite3
+from crosswordhelper import create_setup, hint_generator
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -16,18 +17,13 @@ def getCrossword():
     size = data['size']
 
     db = connect_db()
-    print(db.row_factory)
     crossword = []
     if topic == 'Solar System':
-        planet_attr = db.execute('PRAGMA table_info(Planets)').fetchall()
         planet = db.execute('SELECT * FROM Planets').fetchall()
+        planet = create_setup(planet)
+        planet = hint_generator(planet)
+        print(planet)
 
-
-        # dwarfplanet_attr = db.execute('PRAGMA table_info(DwarfPlanets)').fetchall()
-        # dwarfplanet = db.execute('SELECT * FROM DwarfPlanets').fetchall()
-        # moon_attr = db.execute('PRAGMA table_info(Moons)').fetchall()
-        # moon = db.execute('SELECT * FROM Moons').fetchall()
-        # crossword = planet + dwarfplanet + moon
 
     crossword = planet
     return jsonify(crossword)
--- Backend/crosswordhelper.py
+++ Backend/crosswordhelper.py
@@ -1,13 +1,52 @@
 import random
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from pydantic import BaseModel, Field
 
 
-def create_setup(attr, data):
+
+
+
+def create_setup(data):
+    ''' Return a list of dictionaries in format of {name: name, attr: {attr: value}} '''
 
     setup = []
 
     for row in data:
-        ran = random.randint(2, len(attr) - 1)
-        setup.append({'name': row[1], 'attr': {attr[ran]: row[ran]}})
+        ran = random.randint(2, len(row) - 1)
+        while(not row[list(row.keys())[ran]] or row[list(row.keys())[ran]] == ''):
+            ran += 1
+            if (ran == len(row)):
+                ran = 2
+        setup.append({'name': row['name'], 'attr': {list(row.keys())[ran]: row[list(row.keys())[ran]]}})
     return setup
 
 
+
+def hint_generator(dataSet):
+    ''' Return a list of dictionaries in format of {name: name, hint: hint} '''
+
+    llm = OllamaLLM(model="llama3")
+
+    class Output(BaseModel):
+        name: str = Field(description="The name of the data")
+        hint: str = Field(description="The hint for the data")
+
+    parser = JsonOutputParser(pydantic_object=Output)
+
+    template = """Given the following data which includes the name of the data and an attribute of the data, 
+        generate a hint for the data that would be suitable for a crossword puzzle. The hint should be related to the attribute of the data. 
+        The hint should not include the name of the data.
+
+        You must respond with valid JSON only. Do not include any text before or after the JSON.
+        Put the original name in the name field and the generated hint in the hint field.
+
+        {format_instructions}
+
+        Name: {name}
+        Attribute: {attr}"""
+
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | llm | parser
+    return [chain.invoke({'name': data['name'], 'attr': str(data['attr']), 'format_instructions': parser.get_format_instructions()}) for data in dataSet]
